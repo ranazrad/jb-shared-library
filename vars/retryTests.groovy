6b169a9ecd69857f44a3af31b83eb2d2bfddd107
Ran Azrad <ranazrad@gmail.com>
2025-01-11 19:05:56 +0200

    Update retryTests.groovy

--- vars/retryTests.groovy
+++ vars/retryTests.groovy
@@ -5,7 +5,6 @@ def call(Closure testClosure, int retryCount = 3, boolean demo = false) {
             echo "Simulated retry attempt ${attempt}."
         }
         writeFile file: 'retry-mock-log.txt', text: "Simulated ${retryCount} retry attempts completed."
-        archiveArtifacts artifacts: 'retry-mock-log.txt'
     } else {
         int attempt = 0
         while (attempt < retryCount) {
