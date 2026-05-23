0bf4a6977666dcc536702f42271fb057f11d581f
Ran Azrad <ranazrad@gmail.com>
2026-05-23 22:02:52 +0300

    Delete vars/securityScan.groovy

--- vars/securityScan.groovy
+++
@@ -1,9 +0,0 @@
-def call(Map config = [:]) {
-    echo "Starting Security Vulnerability Scan..."
-    
-    if (config.failOnHighSeverity) {
-        error("CRITICAL VULNERABILITY FOUND! Failing the build as failOnHighSeverity is set to true.")
-    } else {
-        echo "Scan completed successfully. No critical vulnerabilities found or scanner is in audit mode."
-    }
-}
